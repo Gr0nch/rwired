e81a88fbb09886d08dca3ebc0aae3c88cad89ba2
Arlo Pennington <021thehulk120@gmail.com>
2024-04-04 21:02:33 +0000

    Update file constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://movie-web.github.io/links/discord";
-export const GITHUB_LINK = "https://github.com/sussy-code/smov";
-export const TWITTER_LINK = "https://twitter.com/SudoFlix";
+export const DISCORD_LINK = "https://youtu.be/dQw4w9WgXcQ";
+export const GITHUB_LINK = "https://gitlab.com/55yellowcards/the-wired-3";
+export const TWITTER_LINK = "https://youtu.be/dQw4w9WgXcQ";
 export const DONATION_LINK = "https://ko-fi.com/movieweb";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
